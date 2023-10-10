f7da54235d688fdd6ebeeaad20075bc72314f8f9
gtbmulcahey <72265527+gtbmulcahey@users.noreply.github.com>
2023-10-10 13:59:19 -0500

    better styling than before

--- src/people/LargePersonListItem.js
+++ src/people/LargePersonListItem.js
@@ -12,13 +12,13 @@ export const LargePersonListItem = ({ person }) => {
 			<p><span class='title'>Age:</span> {age} years</p>
 			<p><span class='title'>Hair Color:</span> {hairColor}</p>
 			<p><span class='title'>Personality:</span> {personality}</p>
-			<div className="hobbies">
+			<p className="hobbies">
 				<span class='title'>Hobbies:</span>&nbsp;
 				{hobbies && hobbies.map((hobby, i) => [
 					i > 0 && ", ",
-					<div className="hobbies" key={i}>{hobby}</div>
+					<span key={i}>{hobby}</span>
 				])}
-			</div>
+			</p>
 		</>
 	);
 }
