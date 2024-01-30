d94514139b4eaca9c68572a6685396db72ab2843
gtbmulcahey <72265527+gtbmulcahey@users.noreply.github.com>
2024-01-30 12:44:14 -0600

    refining app.js and radio button group

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-
+import React from 'react';
 import './css/App.css';
 import Main from './Main';
 import { printProps } from './printProps';
 import { Header } from './Header';
 import { Footer } from './Footer';
 import './css/LeftHandComponent.css';
-import './css/RightHandComponent.css'
+import './css/RightHandComponent.css';
 
-
-function App(props) {
-  
-  
+// The App component is the root component of the application.
+// It renders the Header, Main, and Footer components.
+const App = () => {
+  // Wrap the Main component with printProps for debugging purposes.
   const MainWrapped = printProps(Main);
 
   return (
@@ -20,6 +20,7 @@ function App(props) {
       <Footer />
     </div>
   );
-}
+};
 
 export default App;
+
--- src/RadioButtonGroup.js
+++ src/RadioButtonGroup.js
@@ -1,47 +1,33 @@
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import './css/RadioButtonGroup.css';
 
+export const RadioButtonGroup = ({ person, possibleQuizAnswers, title, userAnswer = "noUserAnswer", callback }) => {
+  useEffect(() => {
+    console.log(`selectedRadioBtn is ${userAnswer} right now`);
+  }, [person, userAnswer]);
 
-export const RadioButtonGroup = ({ person, possibleQuizAnswers, title, userAnswer, callback }) => {
+  function handleRadioClick(e) {
+    callback(e.currentTarget.value);
+  }
 
-    let selectedRadioBtn = userAnswer;
-
-    useEffect(() => {
-        console.log(`selectedRadioBtn is ${selectedRadioBtn} right now`);
-        console.log(`user answer is ${userAnswer} right now`);
-    }, [person, selectedRadioBtn, userAnswer]);
-
-
-    function handleRadioClick(e) {
-        //setSelectedRadioBtn(e.currentTarget.value);
-        //console.log("about to callback " + e.currentTarget.value);
-        callback(e.currentTarget.value);
-    }
-
-    return (
-        <>
-            <ul className='radiolist'>{title}
-                {
-                    possibleQuizAnswers.map(answer =>
-                        <li key={answer}>
-                            <input
-                                key={answer}
-                                id={answer}
-                                type="radio"
-                                name={answer}
-                                value={answer}
-                                checked={userAnswer === answer}
-                                onChange={handleRadioClick}
-                            />
-                            <label htmlFor={answer}>
-                                {answer}
-                            </label>
-                        </li>
-                    )
-
-
-                }
-            </ul>
-        </>
-    )
-}
+  return (
+    <>
+      <ul className='radiolist'>
+        {title}
+        {possibleQuizAnswers.map(answer => (
+          <li key={answer}>
+            <input
+              id={answer}
+              type="radio"
+              name={answer}
+              value={answer}
+              checked={userAnswer === answer}
+              onChange={handleRadioClick}
+            />
+            <label htmlFor={answer}>{answer}</label>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
